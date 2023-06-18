b73dc2fbfaf9dd00b1dabef36a2526ab5696c907
mabusaki <ferec125@gmail.com>
2023-06-18 16:18:40 +0400

    Before looping

---
+++ main.py
@@ -0,0 +1,62 @@
+import os
+import sys
+import time
+
+from pynput.keyboard import Key, Controller
+#Runs CMD Command to go to given url in chrome.exe
+os.system('cmd /c start chrome https://mxtoolbox.com/SuperTool.aspx')
+time.sleep(3)
+keyboard = Controller()
+listOfDomains = open(r"C:\Users\mabus\Desktop\list1.txt")
+Lines = listOfDomains.readline()
+domain = Lines
+#Clears the searchbox before sending input
+keyboard.press(Key.ctrl)
+keyboard.press('a')
+keyboard.release('a')
+keyboard.release(Key.ctrl)
+time.sleep(1)
+keyboard.press(Key.backspace)
+time.sleep(1)
+#Types spf: + given domain name into the searchbar and hits enter
+
+keyboard.type("spf:")
+time.sleep(1)
+keyboard.type(domain)
+time.sleep(2)
+keyboard.press(Key.enter)
+keyboard.release(Key.enter)
+time.sleep(12)
+#Saves the html page in .txt format
+keyboard.press(Key.ctrl)
+keyboard.press('s')
+keyboard.release('s')
+keyboard.release(Key.ctrl)
+time.sleep(1)
+keyboard.type(domain + ".txt")
+time.sleep(1)
+keyboard.press(Key.enter)
+time.sleep(10)
+#Searches file for any SPF Record
+#file1 = open(r"C:\Users\mabus\Desktop\spfrecords" + "\\" + f'{domain}.txt')
+downloadedSPF = open(r"C:\Users\mabus\Desktop\spfrecords" + "\\" + domain + ".txt", 'r')
+for l_no, line in enumerate(downloadedSPF):
+    if "No SPF Record" in line:
+        print("No SPF Record for domain", domain, "at line", l_no)
+        with open(r"C:\Users\mabus\Desktop\NoSPF.txt", 'a') as noSPF:
+            noSPF.write(domain + "\n")
+        break
+    elif "More than one" in line:
+        print("More than one SPF record found for domain", domain, "at line", l_no)
+        with open(r"C:\Users\mabus\Desktop\MultipleSPFs.txt", 'a') as multipleSPF:
+            multipleSPF.write(domain + "\n")
+        break
+    elif "SPF Record Published" in line:
+        print("SPF Record found for", domain, "at line", l_no)
+        with open(r"C:\Users\mabus\Desktop\SPFfound.txt", 'a') as validSPF:
+            validSPF.write(domain + "\n")
+        break
+
+
+
+
